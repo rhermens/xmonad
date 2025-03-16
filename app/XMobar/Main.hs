5d9ddc14bfef0bf36a70dd779930b0b1d30a45de
Roy <royhermens@hotmail.com>
2025-03-16 17:29:25 +0100

    fmt

--- app/XMobar/Main.hs
+++ app/XMobar/Main.hs
@@ -1,50 +1,85 @@
+import Lib (XResources (..), getXResources)
 import Xmobar
-import Lib (getXResources, XResources(..))
 
 main :: IO ()
 main = do
-    res <- getXResources
-    configFromArgs config >>= xmobar . applyXResources res
+  res <- getXResources
+  configFromArgs config >>= xmobar . applyXResources res
 
 config :: Config
-config = defaultConfig {
-  font = "monospace 11"
-  , additionalFonts = []
-  , borderWidth = 0
-  , border = FullB
-  , alpha = 255
-  , position = TopH 25
-  , textOffset = -1
-  , iconOffset = -1
-  , lowerOnStart = True
-  , pickBroadest = False
-  , persistent = False
-  , hideOnStart = False
-  , iconRoot = "."
-  , allDesktops = True
-  , overrideRedirect = True
-  , textOutputFormat = Ansi
-  , commands = [ Run $ Network "eth0" ["-L","0","-H","32",
-                                        "--normal","green","--high","red"] 10
-               , Run $ Network "eth1" ["-L","0","-H","32",
-                                        "--normal","green","--high","red"] 10
-               , Run $ Cpu ["-L","3","-H","50",
-                             "--normal","green","--high","red"] 10
-               , Run $ Memory ["-t","Mem: <usedratio>%"] 10
-               , Run $ Swap [] 10
-               , Run $ Date "%a %b %_d %Y %H:%M:%S" "date" 10
-               , Run $ Volume "default" "Master" [] 10
-               , Run XMonadLog
+config =
+  defaultConfig
+    { font = "monospace 11",
+      additionalFonts = [],
+      borderWidth = 0,
+      border = FullB,
+      alpha = 255,
+      position = TopH 25,
+      textOffset = -1,
+      iconOffset = -1,
+      lowerOnStart = True,
+      pickBroadest = False,
+      persistent = False,
+      hideOnStart = False,
+      iconRoot = ".",
+      allDesktops = True,
+      overrideRedirect = True,
+      textOutputFormat = Ansi,
+      commands =
+        [ Run $
+            Network
+              "eth0"
+              [ "-L",
+                "0",
+                "-H",
+                "32",
+                "--normal",
+                "green",
+                "--high",
+                "red"
               ]
-  , sepChar = "%"
-  , alignSep = "}{"
-  , template = "%XMonadLog% }{ %default:Master% | %cpu% | %memory% * %swap% | %date%"
-}
+              10,
+          Run $
+            Network
+              "eth1"
+              [ "-L",
+                "0",
+                "-H",
+                "32",
+                "--normal",
+                "green",
+                "--high",
+                "red"
+              ]
+              10,
+          Run $
+            Cpu
+              [ "-L",
+                "3",
+                "-H",
+                "50",
+                "--normal",
+                "green",
+                "--high",
+                "red"
+              ]
+              10,
+          Run $ Memory ["-t", "Mem: <usedratio>%"] 10,
+          Run $ Swap [] 10,
+          Run $ Date "%a %b %_d %Y %H:%M:%S" "date" 10,
+          Run $ Volume "default" "Master" [] 10,
+          Run XMonadLog
+        ],
+      sepChar = "%",
+      alignSep = "}{",
+      template = "%XMonadLog% }{ %default:Master% | %cpu% | %memory% * %swap% | %date%"
+    }
 
 applyXResources :: XResources -> Config -> Config
-applyXResources res cfg = cfg 
-    { --borderColor = background res
-     borderColor = "black"
-    , bgColor = background res
-    , fgColor = foreground res
+applyXResources res cfg =
+  cfg
+    { -- borderColor = background res
+      borderColor = "black",
+      bgColor = background res,
+      fgColor = foreground res
     }
--- app/XMonad/Main.hs
+++ app/XMonad/Main.hs
@@ -3,11 +3,11 @@ import Lib.Media
 import Lib.Workspaces (followTo)
 import Text.Printf (printf)
 import XMonad
+import XMonad.Actions.CycleWS
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Hooks.StatusBar (StatusBarConfig, dynamicEasySBs, statusBarProp)
 import XMonad.Hooks.StatusBar.PP (PP (ppSep))
 import XMonad.Util.EZConfig (additionalKeys)
-import XMonad.Actions.CycleWS
 
 main :: IO ()
 main = do
@@ -26,7 +26,8 @@ cfg =
     `additionalKeys` [ ((kMask, xK_d), spawn "j4-dmenu-desktop --dmenu='dmenu -i -fn \"monospace\" -nb \"#24283b\" -nf \"#c0caf5\" -sb \"#414868\" -sf \"#7aa2f7\"'"),
                        ((kMask, xK_grave), moveTo Next emptyWS),
                        ((kMask .|. shiftMask, xK_grave), followTo Next emptyWS)
-                     ] ++ mediaKeys kMask
+                     ]
+    ++ mediaKeys kMask
 
 barCfg :: PP
 barCfg =
@@ -43,4 +44,3 @@ applyXResources res conf =
     { focusedBorderColor = background res,
       normalBorderColor = background res
     }
-
