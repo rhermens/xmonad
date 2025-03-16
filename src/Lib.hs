33aae15fa06a7811354dfc2025786c0e0a1fd3d6
Roy <royhermens@hotmail.com>
2025-03-16 17:41:14 +0100

    init db once

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,33 +1,34 @@
-module Lib (getXResources, XResources(..), getColor) where
+module Lib (getXResources, XResources (..), getColor) where
 
-import Graphics.X11.XRM (rmGetResource, rmGetFileDatabase, rmValue)
-import System.IO (hPutStr, openFile, IOMode (WriteMode))
+import Graphics.X11.XRM (RMDatabase, rmGetFileDatabase, rmGetResource, rmValue)
+import System.IO (IOMode (WriteMode), hPutStr, openFile)
 import XMonad (rmInitialize)
 
-data XResources = XResources {
-    background :: String,
+data XResources = XResources
+  { background :: String,
     foreground :: String
-} deriving (Show)
+  }
+  deriving (Show)
 
-getColor :: String -> IO String
-getColor color = do
-    db <- rmGetFileDatabase "/home/roy/.Xresources"
-    case db of
-        Just db' -> do
-            res <- rmGetResource db' color "color"
-            case res of
-                Just (_, v) -> rmValue v
-                Nothing -> return "black"
-        Nothing -> return "black"
+getColor :: RMDatabase -> String -> IO String
+getColor db color = do
+  res <- rmGetResource db color "color"
+  case res of
+    Just (_, v) -> rmValue v
+    Nothing -> return "black"
 
 getXResources :: IO XResources
 getXResources = do
-    _ <- rmInitialize
-    bg <- getColor "background"
-    fg <- getColor "foreground"
-    return $ XResources bg fg
+  _ <- rmInitialize
+  db <- rmGetFileDatabase "/home/roy/.Xresources"
+  case db of
+    Just db' -> do
+      bg <- getColor db' "background"
+      fg <- getColor db' "foreground"
+      return $ XResources bg fg
+    Nothing -> return $ XResources "black" "black"
 
 writeStdout :: String -> IO ()
 writeStdout s = do
-    stdout' <- openFile "/home/roy/.xmonad/stdout" WriteMode
-    hPutStr stdout' s
+  stdout' <- openFile "/home/roy/.xmonad/stdout" WriteMode
+  hPutStr stdout' s
